c4c3e43a59ffad1cd2d48ea6a853ab723043f5c5
Tim Riley <tim@openmonkey.com>
2020-12-04 21:53:32 +1100

    Add part 2

--- day01/main.go
+++ day01/main.go
@@ -34,6 +34,17 @@
 //
 // Of course, your expense report is much larger. Find the two entries that sum to 2020;
 // what do you get if you multiply them together?
+//
+// --- Part Two ---
+//
+// The Elves in accounting are thankful for your help; one of them even offers you a
+// starfish coin they had left over from a past vacation. They offer you a second one if
+// you can find three numbers in your expense report that meet the same criteria.
+//
+// Using the above example again, the three entries that sum to 2020 are 979, 366, and
+// 675. Multiplying them together produces the answer, 241861950.
+//
+// In your expense report, what is the product of the three entries that sum to 2020?
 
 package main
 
@@ -60,7 +71,17 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("Part 1 answer: %v", answer1)
+	// 211899
+	fmt.Printf("Part 1 answer: %v\n", answer1)
+
+	answer2, err := part2(nums)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 275765682
+	fmt.Printf("Part 2 answer: %v\n", answer2)
 }
 
 func parseInput(fileName string) (nums []int, err error) {
@@ -114,7 +135,36 @@ func part1(nums []int) (int, error) {
 		return 0, fmt.Errorf("no match found")
 	}
 
-	// Find the answer: 211899
 	answer := match0 * match1
+	return answer, nil
+}
+
+func part2(nums []int) (int, error) {
+	// Search for THREE numbers adding to 2020
+	var match0 int
+	var match1 int
+	var match2 int
+
+	for _, num0 := range nums {
+		for _, num1 := range nums {
+			for _, num2 := range nums {
+				sum := num0 + num1 + num2
+
+				if sum == magicNumber {
+					match0 = num0
+					match1 = num1
+					match2 = num2
+					break
+				}
+			}
+		}
+	}
+
+	// If we've found no matches, then these will be the zero value for ints
+	if match0 == 0 || match1 == 0 || match2 == 0 {
+		return 0, fmt.Errorf("no match found")
+	}
+
+	answer := match0 * match1 * match2
 	return answer, nil
 }
